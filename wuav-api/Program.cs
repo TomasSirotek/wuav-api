1dd6b9bc995a5621fec171b63b61698071abc735
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-05-25 09:50:02 +0200

    have to be cleaned bit more since half of the stuff i made and did not used

--- wuav-api/Program.cs
+++ wuav-api/Program.cs
@@ -33,48 +33,6 @@ if (app.Environment.IsDevelopment())
 
 Dictionary<int, List<string>> TempImages = new Dictionary<int, List<string>>(); // temp saving of the files sent from the swift ui app
 
-// AUTHENTICATE 
-// => authenticate 
-
-app.MapPost("api/authenticate", async (IUserService userService,[FromBody]  AuthenticateRequest request) =>
-{
-    AppUser user = await userService.GetAsyncByEmailAsync(request.Email);
-            
-    if (user == null! || request.Password != "123456")
-       return Results.BadRequest("Email or password is incorrect");
-    
-    return Results.Ok(user);
-}).WithName("Authenticate");
-
-// USERS 
-//  => /users 
-app.MapGet("api/users", async (IUserService userService) =>
-{ 
-    List<AppUser> userList = await userService.GetAllUsersAsync();
-    if (userList.Count == 0)
-        return Results.BadRequest($"Could not find any users");
-    return Results.Ok(userList);
-}).WithName("GetUsers");
-
-// => /userByEmail
-
-app.MapGet("api/users/{email}", async (IUserService userService, string email) =>
-{
-    AppUser user = await userService.GetAsyncByEmailAsync(email);
-    if (user == null)
-        return Results.BadRequest($"Could not find user with email: {email}");
-    return Results.Ok(user);
-}).WithName("GetUserByEmail");
-
-// => /userById
-app.MapGet("api/users/{id:int}", async (IUserService userService, int id) =>
-{
-    AppUser user = await userService.GetUserByIdAsync(id.ToString());
-    if (user == null)
-        return Results.BadRequest($"Could not find user with id: {id}");
-    return Results.Ok(user);
-}).WithName("GetUserById");
-
 // THIS ENDPOINT IS FOR SWIFT UI TO UPLOAD IMAGES TO THE SERVER TEMP
 app.MapPost("api/users/{userId}/images", async (int userId, IFormFile image) =>
 {
